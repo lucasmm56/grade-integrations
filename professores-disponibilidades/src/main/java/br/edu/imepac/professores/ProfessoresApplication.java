f3ce370f465fdfdab381ceeef86545d4cda67b19
lucasmm56 <lucasmm56@gmail.com>
2024-02-21 09:59:15 -0300

    Liberando acesso cors da api para o front

--- professores-disponibilidades/src/main/java/br/edu/imepac/professores/ProfessoresApplication.java
+++ professores-disponibilidades/src/main/java/br/edu/imepac/professores/ProfessoresApplication.java
@@ -2,6 +2,9 @@ package br.edu.imepac.professores;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ProfessoresApplication {
@@ -10,4 +13,17 @@ public class ProfessoresApplication {
 		SpringApplication.run(ProfessoresApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:4200")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+						.allowedHeaders("*")
+						.allowCredentials(true);
+			}
+		};
+	}
 }
